c706d2d8f7d0edf844e4b73e1f68341431cb5b0c
Joel Solano <solano.joel@gmx.de>
2025-04-02 23:28:38 +0200

    chore: basic tests

--- packages/core/src/index.test.ts
+++ packages/core/src/index.test.ts
@@ -8,6 +8,20 @@ describe("tswsrpc", async () => {
 		message: tswsrpc.$event({
 			type: z.string(),
 		}),
+		"middleware.message": client.$event({
+			type: z.object({
+				role: z.string(),
+				error: z.string().optional(),
+			}),
+			use: (data) => {
+				if (data.role !== "admin") {
+					return {
+						...data,
+						error: "UNAUTHORIZED",
+					};
+				}
+			},
+		}),
 	});
 
 	const clientEvents = client.$eventRegistry({
@@ -51,6 +65,38 @@ describe("tswsrpc", async () => {
 
 		expect(serverMessageHandler).toHaveBeenCalledWith(clientToServerMsg);
 		expect(socketMessageHandler).toHaveBeenCalledWith(serverToClientMsg);
+
+		server.$context.server.close();
+	});
+
+	it("should allow basic middlewares", async () => {
+		const server = await $server;
+
+		const serverMessageHandler = vi.fn((data) => {
+			expect(data.role).toEqual("admin");
+		});
+
+		server.on.middleware.message(serverMessageHandler);
+
+		await socket.emit.middleware.message({
+			role: "admin",
+		});
+
+		await new Promise((resolve) => setTimeout(resolve, 100));
+
+		expect(serverMessageHandler).toHaveBeenCalledOnce();
+
+		const serverMessageHandler2 = vi.fn((data) => {
+			expect(data.error).toEqual("UNAUTHORIZED");
+		});
+		server.on.middleware.message(serverMessageHandler2);
+		await socket.emit.middleware.message({
+			role: "user",
+		});
+
+		await new Promise((resolve) => setTimeout(resolve, 100));
+
+		expect(serverMessageHandler2).toHaveBeenCalledOnce();
 
 		server.$context.server.close();
 	});
